bf7466284ae2454328d07b7f300c5028971e1ccf
sebastian.bogado <sebastian.j.bogado@gmail.com>
2017-07-30 18:02:46 -0300

    feat(server): fix mock

---
+++ client/src/App/Item/index.js
@@ -0,0 +1,5 @@
+import Header from './Header.jsx';
+import Container from './Header.container.js';
+
+export { Header };
+export default Container;
--- server/index.js
+++ server/index.js
@@ -56,7 +56,9 @@ app.get('/api/items/:id', function (req, res) {
       },
       picture: "http://mla-s1-p.mlstatic.com/977901-MLA20426607586_092015-I.jpg",
       condition: "new",
-      free_shipping: false
+      free_shipping: false,
+      sold_quantity: 155,
+      description: '<table width="900" border="0" cellpadding="0" cellspacing="0"><tbody><tr><td><img src="http://www.serviciospcsi.com.ar/graficas/mecafron/header.gif" height="227" width="900" /></td></tr><tr><td><img src="http://www.serviciospcsi.com.ar/graficas/mecafron/ipod touch 2016.jpg" height="4000" width="900" /></td></tr><tr><td><img src="http://www.serviciospcsi.com.ar/graficas/mecafron/mercado.jpg" height="232" width="900" /></td></tr><tr><td><img src="http://www.serviciospcsi.com.ar/graficas/mecafron/envios.gif" height="400" width="900" /></td></tr><tr><td><img src="http://www.serviciospcsi.com.ar/graficas/mecafron/facebook.gif" height="425" width="900" /></td></tr><tr><td><img src="http://www.serviciospcsi.com.ar/graficas/mecafron/pie.jpg" height="794" width="900" /></td></tr></tbody></table>'
     }
   });
 });
